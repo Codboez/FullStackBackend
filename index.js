c8fa7f86bdfee573216760c12dc5ccc670b55898
Jerry Lehtinen <jerry.lehtinen02@gmail.com>
2023-08-09 09:34:28 +0300

    Added database

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const morgan = require("morgan")
 const app = express()
 const cors = require("cors")
+require("dotenv").config()
 
 const PORT = process.env.PORT || 3001
+
+app.use(express.static("build"))
 
 app.use(express.json())
 
@@ -12,93 +15,91 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :r
 
 app.use(cors())
 
-app.use(express.static("build"))
+const mongo = require("./mongo")
 
-people = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
-app.get("/api/people", (request, response) => {
-  response.json(people)
+app.get("/api/people", (request, response, next) => {
+  mongo.get_people()
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
-app.get("/info", (request, response) => {
-  let info = `Phonebook has info for ${people.length} people\n\n`
+app.get("/info", (request, response, next) => {
+  mongo.get_people_count().then(result => {
+    let info = `Phonebook has info for ${result} people\n\n`
 
-  const date = new Date()
-  info += date.toString()
+    const date = new Date()
+    info += date.toString()
 
-  response.send(info)
+    response.send(info)
+  }).catch(error => next(error))
 })
 
-app.get("/api/people/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = people.find(p => p.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get("/api/people/:id", (request, response, next) => {
+  const id = request.params.id
+  
+  mongo.get_person(id).then(result => {
+    if (result) {
+      response.json(result)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
-app.delete("/api/people/:id", (request, response) => {
-  const id = Number(request.params.id)
-  people = people.filter(person => person.id !== id)
+app.delete("/api/people/:id", (request, response, next) => {
+  const id = request.params.id
 
-  response.status(204).end()
+  mongo.delete_person(id)
+    .then(() => {response.status(204).end()})
+    .catch(error => next(error))
 })
 
-app.post("/api/people", (request, response) => {
-  const content = request.body
-  let person = {name: "", number: "", id: -1}
+app.post("/api/people", (request, response, next) => {
+  mongo.get_people().then(result => {
+    let person = {name: request.body.name, number: request.body.number}
 
-  console.log(content.name)
-  console.log(content.name === true)
-  console.log(typeof content.name !== "string")
-  console.log(typeof content.name)
+    /*if (typeof content.name !== "string" || content.name === "") {
+      return response.status(400).send("{ error: 'Invalid name' }")
+    } else {
+      person.name = content.name
+    }*/
 
-  if (typeof content.name !== "string" || content.name === "") {
-    return response.status(400).send("{ error: 'Invalid name' }")
-  } else {
-    person.name = content.name
-  }
+    if (result.find(p => p.name === person.name)) {
+      return response.status(400).send("{ error: 'Name already exists' }")
+    }
 
-  if (people.find(p => p.name === person.name)) {
-    return response.status(400).send("{ error: 'Name already exists' }")
-  }
+    /*if (typeof content.number !== "string" || content.number === "") {
+      return response.status(400).send("{ error: 'Invalid number' ")
+    } else {
+      person.number = content.number
+    }*/
 
-  if (typeof content.number !== "string" || content.number === "") {
-    return response.status(400).send("{ error: 'Invalid number' ")
-  } else {
-    person.number = content.number
-  }
-
-  person.id = Math.floor(Math.random() * Math.pow(10, 9))
-
-  people = people.concat(person)
-
-  response.send(person)
+    mongo.add_person(person)
+      .then(new_person => {response.send(new_person)})
+      .catch(error => next(error))
+  })
 })
+
+app.put("/api/people/:id", (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+
+  mongo.update_number(id, body)
+    .then(result => {response.json(result)})
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  if (error.name === "CastError") {
+    return response.status(400).send("{ error: 'Invalid id'}")
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send(error.message)
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
